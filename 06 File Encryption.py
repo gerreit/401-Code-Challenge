ebb84181f0e181e0ef4b87cca697a1d9d90c9d54
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-09-24 13:32:44 -0400

    Update 06 File Encryption.py

--- 06 File Encryption.py
+++ 06 File Encryption.py
@@ -50,6 +50,7 @@ def load_key():
 
 # defines a function to encrypt messages
 def encrypt_message():
+    message = input("Please type the message to encode: ")
     # takes the message user inputted variable and encrypts it
     message.encrypt()
     # initialize the Fernet class (taken from site dont quite understand it)
@@ -57,41 +58,56 @@ def encrypt_message():
     # stores the encrypted message as a variable 
     encrypted = f.encrypt(message)
 
+    print("Here's your encrypted message")
+    print(encrypted)
 def decrypt_message():
+    message = input("Message to decrypt")
+    decrypted = str.encode(message)
     # initialize the Fernet class (taken from site dont quite understand it)
     f = Fernet(key)
     # save decrypting message as a variable
-    decrypted_encrypted_message = f.decrypt(message)
-
+    decrypted_encrypted_message = f.decrypt(decrypted)
+    print(decrypted_encrypted_message)
 def encrypt(filename, key):
     f = fernet(key)
+    path = input("Input file path of file to encrypt")
     with open(filename, "rb") as file:
         
         file_data = file.read()
-        encrypted_data = f.encrypt(file_data)
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
 
+def decrypt():
+    f = Fernet(key)
+    path = input("path of file to decrypt")
+    with open (filename, "rb") as file:
+        file_data = file.read()
+    decrypted = f.decrypt(file_data)
+    with open (filename, "wb") as file:
+        file.write(decrypted)
 
 # Variables:
 
 # stores load key function as a variable
 key = load_key
 
-# 
-menu = input("Press 1 for thing\nPress 2 for thing\n Press 3 for thing\n Press 4 for thing")
 
-# the message stored as a variable
-message = input("Please type the message to encode: ")
+menu = input("Press 1 to encrypt a file\nPress 2 to decrypt a file\n Press 3 to encrypt a message\n Press 4 to decrypt a message")
+
+
+
 
 # Main:
 
-if menu == 1():
-    pass
-elif
-    pass
-elif       
-    pass
-elif    
-    pass
+if (menu == "1"):
+    encrypt()
+elif (menu == "2"):
+    decrypt()
+elif (menu == "3"):   
+    encrypt_message()
+elif (menu == "4"):
+    decrypt_message()
 else
     print("Not a valid input")
 
