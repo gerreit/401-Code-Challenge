fc17311d4b903cb1ebfec8b6f628539e99f24774
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-09-26 17:17:59 -0400

    Update 11-Network-Security.py

--- 11-Network-Security.py
+++ 11-Network-Security.py
@@ -19,6 +19,7 @@
 # Sources:
 # https://scapy.readthedocs.io/en/latest/extending.html
 # ChatGPT helped with the for loop part
+# marco code
 
 # Imported Libraries:
 import sys
@@ -26,32 +27,43 @@ from scapy.all import sr1,IP,TCP,ICMP
 
 
 # Variables:
-host_name = "scanme.nmap.org"
+
+# same as above but for a  destionation port on the hostinstead
 destination_port = input("Input a destination port")
+# stores the port you want your traffic to come from as a variable
 source_port = input("Input a source port")
+# range of ports to scan. for this one its just as singe port, dont think this is actually used in the code though
 port_range = 22
-start_port = input("Input a starting port:")
-end_port = input("Input a end port:")
+# which port you want to start the scan at as a variable
+start_port = int(input("Input a starting port:"))
+# which port you want to end the scan on as a variable
+end_port = int(input("Input a end port:"))
 
 # Functions:
-def test_ports(start_port, end_port):
+def test_ports():
     target_ip = "localhost"  # Replace with the target IP address
+    # stores the name of the host to be scanned into a variable
+    host_name = "scanme.nmap.org"
 
+    # for each port in the range of the start port and end port do...
     for port in range(start_port, end_port + 1):
-        # Create an IP and TCP layer packet with destination port as 'port'
-        packet = IP(dst=target_ip)/TCP(dport=port, flags='S')
+        # Create an packet that is sent to the host destination port
+        packet = IP(dst=host_name)/TCP(dport=port, flags='S')
 
         # Send the packet and wait for a response
         response = sr1(packet, timeout=1, verbose=False)
 
+        # if the packet has a tcp packet then 
         if response and response.haslayer(TCP):
-            if response[TCP].flags == "SA":
+            if response[TCP].flags == 18:
                 print(f"Port {port} is open.")
-                # Send a RST packet to graciously close the open connection
+                # closes the open connection
                 rst_packet = IP(dst=target_ip)/TCP(dport=port, sport=response[TCP].dport, flags='R')
                 send(rst_packet, verbose=False)
-            elif response[TCP].flags == "RA":
+                # if the port is closed it will return a 4 which means its closed
+            elif response[TCP].flags == 4:
                 print(f"Port {port} is closed.")
+                # if not 4 or 18 then it will print this 
             else:
                 print(f"Port {port} is filtered and silently dropped.")
         else:
@@ -63,10 +75,7 @@ def test_ports(start_port, end_port):
 
 print("Make sure you run this script as sudo")
 
-#p=sr1(IP(dst=sys.argv[1])/ICMP())
-#if p:
-   # p.show()
 
-#print (response)
 
-test_ports
+
+test_ports()
