64d5ad8068b83221d469395eb38371467ddbf06c
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-09-26 18:57:34 -0400

    Update 31-DetectOS.py

--- 31-DetectOS.py
+++ 31-DetectOS.py
@@ -7,8 +7,8 @@
 # Instructions:
 
 # Prompt the user to type in a file name to search for.
-#  input variable for file name
-# Prompt the user for a directory to search in 
+# input variable for file name
+# Prompt the user for a directory to search in.
 #  input a variable for file path
 # Search each file in the directory by name.
 #  need to look this up, it reminds me of the file.readline from a few weeks ago
@@ -21,24 +21,39 @@ import os
 import platform
 from time import sleep
 
-# A lil help from ChatGPT
 # Functions:
 def Linux_FileSearch():
+    # store the file name and path
     File_Name = input("Input a file name to search for: ")
+    # store the file path
     File_Path = input("Input a file path: ")
+    # store the number of files searched
     Files_Searched = 0
+    # store the number of hits
     Confirmed = 0
-    for root, dir, files in os.walk(directory):
-        Files_Searched += 1
-        if file == File_Name:
-            Confirmed += 1
-            print(f"Found {file} in {File_Path}")
+    # will walk through the directory and search for the file
+    for root, dirnames, filenames in os.walk(File_Path):
+        # for each file in the directory,
+        for filename in filenames:  
+            # for each file in the directory, add the file name to the list of files searched
+            Files_Searched += 1
+            # if the file name is the same as the inputted file name
+            if filename == File_Name: 
+                # add one file confirmed 
+                Confirmed += 1
+                # print to the screen the file name and location
+                print(f"Found {File_Name} in {File_Path}")
 
-    return Files_Searched, hits
-def_Windows_FileSearch():
+    return Files_Searched, Confirmed
+
+# portion from marcos code
+def Windows_FileSearch():
     File_Name = input("Input a file name to search for: ")
-    File_Path = input("Input a file path: ")
-
+    Directory = input("Input a directory: ")
+    Files_Searched = 0
+    # uses os.system to run windows specific commands to look for specified files . This looks like a bunch of gibberish to me honestly
+    searchcount = os.system("dir /a:-d /s /b " + str(Directory) + "\\" + str(whichFile) + " | find /c \":\\\"").read()
+    print("Found files: " + Files_Searched)
 
 # Variables:
 check_OS = platform.system()
