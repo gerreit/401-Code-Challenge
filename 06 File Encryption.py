d2515b17e92835815fb62cba7a015cd3aafed4e5
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-09-27 13:35:05 -0400

    Update 06 File Encryption.py

--- 06 File Encryption.py
+++ 06 File Encryption.py
@@ -35,82 +35,87 @@
 # Imported Libraries:
 from cryptography.fernet import Fernet
 
-# Functions:
+# makes key for encryption/decryption
 def make_key():
-    # stores generated key as a variable
+    # saves Fernet.generate_key() as a variable, this is to make this easier to call and for cleaner looking code
     key = Fernet.generate_key()
-    # Open the file named "key.key" in binary mode ("wb") to write the key
+    # with your key open as a key file write in binary a generated key
     with open("key.key", "wb") as key_file:
-        # Write the key to the file in binary format
         key_file.write(key)
 
+# function to load previously made keys
 def load_key():
-    # opens and reads in binary format then returns the contents of the file
     return open("key.key", "rb").read()
 
-# defines a function to encrypt messages
-def encrypt_message():
-    message = input("Please type the message to encode: ")
-    # takes the message user inputted variable and encrypts it
-    message.encrypt()
-    # initialize the Fernet class (taken from site dont quite understand it)
-    f = Fernet(key)
-    # stores the encrypted message as a variable 
-    encrypted = f.encrypt(message)
 
-    print("Here's your encrypted message")
-    print(encrypted)
-def decrypt_message():
-    message = input("Message to decrypt")
-    decrypted = str.encode(message)
-    # initialize the Fernet class (taken from site dont quite understand it)
+# calls the function to load a previously made key, you would need it to decrypt already encrypted files
+key = load_key()
+
+# makes a function for encrypting messages
+def encrypt_message():
+    # takes a user inputted message and encodes it and is stored as a variable
+    message = input("Please type the message to encode: ").encode()
+    # honestly no clue, saves it as a variable though
     f = Fernet(key)
-    # save decrypting message as a variable
-    decrypted_encrypted_message = f.decrypt(decrypted)
+    # encrypts the message variable and saves it as a different variable
+    encrypted = f.encrypt(message)
+    print("Your encrypted message is:  ")
+    print(encrypted)
+
+# function for decrypting messages
+def decrypt_message():
+    # input a decrypted message here and it gets saved as a variable
+    message = input("Message to decrypt: ").encode()
+    # see previous
+    f = Fernet(key)
+    # decrypts the message variable and saves it as different variable
+    decrypted_encrypted_message = f.decrypt(message)
+    print("Decrypted message:")
     print(decrypted_encrypted_message)
-def encrypt(filename, key):
-    f = fernet(key)
-    path = input("Input file path of file to encrypt")
-    with open(filename, "rb") as file:
-        
+
+# function for encrypting a file
+def encrypt():
+    # see previous
+    f = Fernet(key)
+    # this takes the file path of the file you wanna encrypt and saves it as a variable
+    path = input("Input file path of file to encrypt: ")
+    # with the filepath open read it as binary as the variable file
+    with open(path, "rb") as file:
+        # reads the file at the end of the file path and saves it as variable file_data
         file_data = file.read()
+    # takes the file_data variable and encrypts it then saves it as encrypted_data variable
     encrypted_data = f.encrypt(file_data)
-    with open(filename, "wb") as file:
+    # with the file path open write in binary as file
+    with open(path, "wb") as file:
+        # this does all the encrypting stuff
         file.write(encrypted_data)
 
+# defines a function for decrypting a file
+# if your decrypting a file previously encrypted with a different key make sure you load that key or else this wont work properly (if at all)
 def decrypt():
+    # see previous
     f = Fernet(key)
-    path = input("path of file to decrypt")
-    with open (filename, "rb") as file:
+    # saves the file path of the file to decrypt as variable path
+    path = input("Path of file to decrypt: ")
+    # with the file path open read as binary as variable file
+    with open(path, "rb") as file:
+        # reads the file path and saves it as variable file_data
         file_data = file.read()
+    # decrypts the file data and saves it as variable decypted
     decrypted = f.decrypt(file_data)
-    with open (filename, "wb") as file:
+    # with the file path open write in binary the decrypted text into the file
+    with open(path, "wb") as file:
         file.write(decrypted)
 
-# Variables:
-
-# stores load key function as a variable
-key = load_key
-
-
-menu = input("Press 1 to encrypt a file\nPress 2 to decrypt a file\n Press 3 to encrypt a message\n Press 4 to decrypt a message")
-
-
-
-
-# Main:
-
-if (menu == "1"):
-    encrypt()
-elif (menu == "2"):
-    decrypt()
-elif (menu == "3"):   
+# stuff for making a menu to select which option you wanna use
+option = input("Press 1 to encrypt message\nPress 2 to decrypt message\n Press 3 to encrypt file\n Press 4 to decrypt file: ")
+if option == '1':
     encrypt_message()
-elif (menu == "4"):
+elif option == "2":
     decrypt_message()
-else
-    print("Not a valid input")
-
-# calls the make_key function defined earlier
-make_key
-
+elif option == "3":
+    encrypt()
+elif option == "4":
+    decrypt()
+else:
+    print("Error happened")
