24735b00dd71f0764a1a74daa129e4391e170afc
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-09-28 14:43:19 -0400

    Update 02 uptime sensor.py

--- 02 uptime sensor.py
+++ 02 uptime sensor.py
@@ -25,22 +25,23 @@ import smtplib
 # Variables:
 Infinite = 1
 destination = input("Enter a DNS server to ping: ")
-#email = input("Enter in your email: ")
-#password = getpass("Enter in your password, nothing visual will happen when inputting for security: ")
 
 # Functions:
 def uptime_sensor():
     while Infinite == 1:
         response = os.system("ping -c 1 " + destination)
-        time.sleep(2)
         if response == 0:
             DNS = "Host is up"
         else:
             DNS = "Host is down"
-        return DNS
-        print(response + datetime.datetime.now + destination)
+        timestamp = datetime.datetime.now()
+        print(f"{timestamp} - Destination: {destination} - Status: {DNS}")
+        time.sleep(2)
+
+
 
 uptime_sensor()
+
 
 
 # i cant figure out why this loop and keep pinging
